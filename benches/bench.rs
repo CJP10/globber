d8bd2e0e23882fee346fb18c962b09427759ef0f
Connor Peticca <cjpet10@gmail.com>
2019-06-01 21:50:08 -0400

    remove jemalloc

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,36 +1,31 @@
 extern crate criterion;
-extern crate globber;
-extern crate jemallocator;
 
 use criterion::*;
 
 use globber::Pattern;
 
-#[global_allocator]
-static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
-
 fn glob_benchmark(c: &mut Criterion) {
-    c.bench("matches | ",
+    c.bench("",
             Benchmark::new("some/**/**/needle.txt", |b| {
             let p = Pattern::new("some/**/**/needle.txt").unwrap();
-            b.iter(|| black_box(p.matches("some/one/two/needle.txt")));
+                b.iter(|| p.matches("some/one/two/needle.txt"));
         }).throughput(Throughput::Bytes("some/one/two/needle.txt".len() as u32)),
     );
-    c.bench("matches | ",
+    c.bench("",
             Benchmark::new("a*a*a*a*a*a*a*a*a", |b| {
                 let p = Pattern::new("a*a*a*a*a*a*a*a*a").unwrap();
-                b.iter(|| black_box(p.matches("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")));
+                b.iter(|| p.matches("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"));
             }).throughput(Throughput::Bytes("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa".len() as u32)),
     );
-    c.bench("matches | ",
+    c.bench("",
             Benchmark::new("*hello.txt", |b| {
                 let p = Pattern::new("*hello.txt").unwrap();
-                b.iter(|| black_box(p.matches("gareth_says_hello.txt")));
+                b.iter(|| p.matches("gareth_says_hello.txt"));
             }).throughput(Throughput::Bytes("gareth_says_hello.txt".len() as u32)),
     );
-    c.bench("matches | ",
+    c.bench("",
             Benchmark::new("!(+(secret|private)*+(.jpg|.gif))", |b| {
-                let p = Pattern::new("!(+(a|b)*+(.jpg|.gif))").unwrap();
+                let p = Pattern::new("!(+(secret|private)*+(.jpg|.gif))").unwrap();
                 b.iter(|| p.matches("secret_image.png"));
             }).throughput(Throughput::Bytes("secret_image.png".len() as u32)),
     );
